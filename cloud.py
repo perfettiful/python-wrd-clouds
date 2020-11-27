4708ec6009bd51d4e5993ef5d52f73eeb710a256
Nate <nathanperfetti@gmail.com>
2020-11-26 20:23:14 -0600

    virtual env created

---
+++ cloud.py
@@ -0,0 +1,7 @@
+import pathlib
+from wordcloud import WordCloud, STOPWORDS, ImageGenerator
+import matplotlib.pyplot as pyplot
+import numpy as numpy
+from PIL import ImageGenerator
+%matplotlib inline
+
